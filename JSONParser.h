5e35565746006aad4824cebaf544763efd79b9e5
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-12-04 23:28:59 +0100

    Setting file object as ofstream cause we want only read data, nothing to change/input.

--- JSONParser.h
+++ JSONParser.h
@@ -1,10 +1,17 @@
+#include <string>
+#include <fstream>
+
+
 namespace jp {
     class JSONParser {
         public:
+            JSONParser(std::string& file);
 
         protected:
 
         private:
+            std::string fileName;
+            std::ofstream file;
 
     };
 }
