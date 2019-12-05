52666128822cabb52e4eb6f8bc5354be921e8aed
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-12-05 23:42:01 +0100

    Declare function`s for parsing data called: openFile, checkSyntax, parseData.

--- JSONParser.h
+++ JSONParser.h
@@ -4,14 +4,25 @@
 #include "JSONElement.h"
 
 namespace jp {
+    enum class JSONError {
+        SYNTAX_ERROR,
+        NO_DATA,
+        NO_FILE,
+        DUPLICATE
+    };
+
     class JSONParser {
         public:
             JSONParser(std::string& file);
 
-
             JSONElement* parseFile();
 
         protected:
+            JSONError openFile();
+
+            JSONError checkSyntax();
+
+            JSONError parseData();
 
         private:
             std::string fileName;
